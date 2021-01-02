23f3dd23990f1f3fedd1c93d16535780d646189b
aida <madjidiene@gmail.com>
2021-01-03 00:52:43 +0100

    first commit

---
+++ src/main/java/com/tdsi/TdsiApplication.java
@@ -0,0 +1,13 @@
+package com.tdsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdsi/TdsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
